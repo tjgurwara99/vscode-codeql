20e7626919805497c084c757ff4cb30078b56ea1
Taj <tjgurwara99@github.com>
2022-12-14 20:38:13 +0000

    separation of concern of creating a PR vs code to change the version

--- scripts/replace-cli-version.sh
+++ scripts/replace-cli-version.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-VERSIONS=$(gh api -H "Accept: application/vnd.github+json" /repos/github/codeql-cli-binaries/releases | jq '.[].tag_name' | head -2)
+VERSIONS=$(gh api -H "Accept: application/vnd.github+json" /repos/github/codeql-cli-binaries/releases | jq -r '.[].tag_name' | head -2)
 
-LATEST_VERSION=$(echo $VERSIONS | awk '{ print $1 }' | sed "s/\"//g")
-PREVIOUS_VERSION=$(echo $VERSIONS | awk '{ print $2 }' | sed "s/\"//g")
+# we are exporting these variables so that we can access these variables in the workflow
+export LATEST_VERSION=$(echo $VERSIONS | awk '{ print $1 }')
+export PREVIOUS_VERSION=$(echo $VERSIONS | awk '{ print $2 }')
 
 sed -i "s/$PREVIOUS_VERSION/$LATEST_VERSION/g" .github/workflows/main.yml
 sed -i "s/$PREVIOUS_VERSION/$LATEST_VERSION/g" extensions/ql-vscode/src/vscode-tests/ensureCli.ts
